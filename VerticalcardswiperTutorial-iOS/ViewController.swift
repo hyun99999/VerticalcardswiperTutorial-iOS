af70ced2fdcbfd9ba192d4ec1a82badece193f06
hyungyu Kim <ypsophila97@icloud.com>
2021-08-04 23:59:42 +0900

    [chore] project 설정 및 VerticalCardSwiper pod install

---
+++ VerticalcardswiperTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerticalcardswiperTutorial-iOS
+//
+//  Created by kimhyungyu on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
